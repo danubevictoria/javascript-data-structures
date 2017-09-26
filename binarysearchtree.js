68e1d8f775a8f71d7e861ddad7e0840bbfd5080d
unknown <danubevictoria@gmail.com>
2017-09-25 23:40:10 -0700

    bst

--- binarysearchtree.js
+++ binarysearchtree.js
@@ -1,4 +1,5 @@
 // Binary Search Tree
+// O (log n) faster than linked list because you only have to search through half of the tree to find the one you are looking for
 // Constructors
 function BST(value) {
   this.value = value;
